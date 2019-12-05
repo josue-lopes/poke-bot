e7fb585b90865f6a4426887b301e2c132fbb9297
josue-lopes <josue.lopes258@gmail.com>
2019-12-05 16:59:11 -0500

    Switched application over to Spring Boot

--- src/main/java/com/josuelopes/bot/pokebot/App.java
+++ src/main/java/com/josuelopes/bot/pokebot/App.java
@@ -2,15 +2,19 @@ package com.josuelopes.bot.pokebot;
 
 import com.josuelopes.bot.pokebot.services.BotService;
 
-//TODO: migrate to Spring Boot
-public final class App 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App 
 {
-    private App() 
+    public App() 
     {
     }
     
     public static void main(String[] args) 
     {
+        SpringApplication.run(App.class, args);
         BotService service = new BotService();
         service.startBot();
     }
